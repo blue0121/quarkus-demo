2679d926efc13a8a7e099fa61f946050f88dd71c
Jin Zheng <89609719@qq.com>
2022-03-29 14:24:39 +0800

    增加本地打包验证

--- quarkus-web/src/test/java/demo/quarkus/web/resource/HelloResourceTest.java
+++ quarkus-web/src/test/java/demo/quarkus/web/resource/HelloResourceTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import java.util.UUID;
 
 import demo.quarkus.web.service.HelloService;
+import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectSpy;
 import io.restassured.RestAssured;
@@ -32,6 +33,16 @@ public class HelloResourceTest {
     }
 
     @Test
+    public void testGreeting() {
+        String uuid = UUID.randomUUID().toString();
+        RestAssured.given().pathParam("name", uuid).when().get("/hello/greeting/{name}").then()
+                .statusCode(200)
+                .body("name", Matchers.is(uuid))
+                .body("message", Matchers.is("Hello," + uuid));
+    }
+
+    @DisabledOnNativeImage
+    @Test
     public void testGreetingMock() {
         String uuid = UUID.randomUUID().toString();
         Mockito.when(helloService.greeting(Mockito.eq(uuid))).thenReturn("Hello");
@@ -39,14 +50,5 @@ public class HelloResourceTest {
                 .statusCode(200)
                 .body("name", CoreMatchers.is(uuid))
                 .body("message", CoreMatchers.is("Hello"));
-    }
-
-    @Test
-    public void testGreeting() {
-        String uuid = UUID.randomUUID().toString();
-        RestAssured.given().pathParam("name", uuid).when().get("/hello/greeting/{name}").then()
-                .statusCode(200)
-                .body("name", Matchers.is(uuid))
-                .body("message", Matchers.is("Hello," + uuid));
     }
 }
